881b0d3e8bb8d82916d10ae380a42be2baf5af38
sagar <070bct535@ioe.edu.np>
2019-08-23 13:55:44 +0545

    deciphers a text

--- ceaser_cipher_string.py
+++ ceaser_cipher_string.py
@@ -20,3 +20,21 @@ class Ceaser_cipher_string:
 				j+=1
 
 		return ''.join(ciphered_array)
+
+	def decipherString(self, key, ciphered_string):
+		array = []
+		j = 0
+
+		for i in range(0, len(ciphered_string)):
+			length = len(key)
+
+			r = i%length
+
+			if (r==0):
+				j = 0
+				array.append(cs.deCipher(key[j], ciphered_string[i]))
+			else:
+				array.append(cs.deCipher(key[i], ciphered_string[i]))
+				j += 1
+
+		return ''.join(array)
